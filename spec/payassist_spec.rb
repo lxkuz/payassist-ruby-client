9be66ebb175ace77def520f13c454d27048ba573
Alexey Kuznetsov <a.kuznetsov@okft.io>
2024-04-08 19:59:27 +0700

    Remove useless failing test

--- spec/payassist_spec.rb
+++ spec/payassist_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Payassist do
   it "has a version number" do
     expect(Payassist::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
